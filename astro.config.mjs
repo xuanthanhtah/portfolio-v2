9f4386faad48b0ae30e648aac6d15895723a44d8
Xuan Thanh <thanh26092000@gmail.com>
2026-02-03 16:24:58 +0700

    This version of Antigravity is no longer supported. Please update to receive the latest features!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
+import vue from '@astrojs/vue';
+
+import svelte from '@astrojs/svelte';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), vue(), svelte()]
+});
